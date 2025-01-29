9e5c152264b15529fab72ba92823fbdacefbcab7
geer26 <gergo.kurucz@mobilegap.com>
2025-01-29 13:35:42 +0100

    bugfix_1

--- staircase_problem/threaded/solution.go
+++ staircase_problem/threaded/solution.go
@@ -2,15 +2,15 @@ package threaded
 
 import (
 	"fmt"
-	"sync"
 )
 
-// Típusdefiníció
+// Egy variávió reprezentációja
+type varia []int
+
+// Struct definíció
 type permutations struct {
-	variations [][]int // A lépések tárolása, 2 dimenziós slice
+	variations []varia // A lépések tárolása, 2 dimenziós slice
 	levels     int     // A megmászandő lépcsők száma
-	mu         sync.Mutex
-	wg         sync.WaitGroup
 }
 
 // Típusmetódus az eredmény kiírására
@@ -20,77 +20,85 @@ func (p permutations) Show_permutations() {
 		return
 	}
 	fmt.Printf("%d lépcsőfokot %d féleképpen lehet megmászni:\n", p.levels, len(p.variations))
-	for i, perm := range p.variations {
-		fmt.Printf("%d. - [%s]\n", i+1, perm)
+	for i, variation := range p.variations {
+		fmt.Printf("%d. - %d\n", i+1, variation)
 	}
 }
 
-// Elértük a lépcsők tetejét?
-func (p permutations) check_if_on_top(steps []int) bool {
+// Szintjelző. ha negatív, akkor még nem értünk fel
+// ha 0, akkor pont a tetjén állunk, ha pozitív, akkor
+// túlmentünk
+func (p *permutations) leveling(variation varia) int {
 	current_level := 0
-	for _, steps := range steps {
-		current_level += steps
+	for _, step := range variation {
+		current_level += step
 	}
-	if current_level == p.levels {
-		return true
-	}
-	return false
-}
-
-// Túlmentünk?
-func (p permutations) overdid_levels(steps []int) bool {
-	current_level := 0
-	for _, steps := range steps {
-		current_level += steps
-	}
-	if current_level > p.levels {
-		return true
-	}
-	return false
+	return current_level - p.levels
 }
 
 // A rekurzív worker
-func (p permutations) calculate(variation []int) {
-	if p.overdid_levels(variation) {
-		p.wg.Done()
-		return
-	}
-	if p.check_if_on_top(variation) {
-		p.mu.Lock()
-		p.variations = append(p.variations, variation)
-		p.mu.Unlock()
-		p.wg.Done()
-		return
+func (p *permutations) worker(variation varia) {
+
+	// 1. meghatározzuk, hol állunk
+	leveling := p.leveling(variation)
+
+	switch leveling == 0 {
+	// Ha éppen a tetején, akkor rögzítjük az aktuális variációt és kilépünk
+	case true:
+		{
+			p.variations = append(p.variations, variation)
+			return
+		}
+	case false:
+		// Ha nem pont a tetején tovább vizsgálódunk
+		{
+			switch leveling > 0 {
+			// Ha túlmentünk, biztosan rossz a variáció
+			case true:
+				return
+			// Ha túl alacsony, további variációkat generálunk, és új rekurzív workereket indítunk
+			case false:
+				{
+					p.worker(append(variation, 1))
+					p.worker(append(variation, 2))
+				}
+			}
+		}
 	}
 
-	one_new_step := append(variation, 1)
-	two_new_steps := append(variation, 2)
-	p.wg.Add(2)
-	go p.calculate(one_new_step)
-	go p.calculate(two_new_steps)
-	p.wg.Wait()
+	/*
+	   // Ha túlmentünk, állítsuk le a rutint
+
+	   	if p.above_levels(variation) {
+	   		return
+	   	}
+
+	   // Ha a tetjén vagyunk, jó a megoldás, rögzítsük a variációt és állítsuk le a rutint
+
+	   	if p.check_if_on_top(variation) {
+	   		return
+	   	}
+
+	   // Ha még nem  értünk fel, két új ággal hívjuk újra a worker-t
+
+	   	if p.below_levels(variation) {
+	   		p.worker(append(variation, 1))
+	   		p.worker(append(variation, 2))
+	   		return
+	   	}
+	*/
 }
 
-// Megoldások kiszámítása
-func (p permutations) calc_permutations() {
-	if p.levels == 0 {
-		return
-	}
-
-	var initial []int
-	var one_new_step = append(initial, 1)
-	var two_new_steps = append(initial, 2)
-	p.wg.Add(2)
-	go p.calculate(one_new_step)
-	go p.calculate(two_new_steps)
-
-	p.wg.Wait()
-}
-
-// Inicializáslás
+// Inicializálás
 func NewPermutations(levels int) *permutations {
 	p := new(permutations)
 	p.levels = levels
-	p.calc_permutations()
+	p.variations = make([]varia, 0)
+
+	var variation = make(varia, 0)
+
+	p.worker(append(variation, 1))
+	p.worker(append(variation, 2))
+
 	return p
 }
