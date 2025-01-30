feb168f1d8a1a92f222eb4f2fe0a4e3d11436829
halm4d <halma.david.88@gmail.com>
2025-01-30 17:24:59 +0100

    halm4d's solution to find the longest common substring -- readSteps function test added with environment variables

--- halm4d/issue2/main_test.go
+++ halm4d/issue2/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -18,6 +19,27 @@ func TestSolveStaircase(t *testing.T) {
 
 	for _, test := range tests {
 		result := solveStaircase(test.input)
+		if result != test.output {
+			t.Errorf("Expected %d, got %d", test.output, result)
+		}
+	}
+}
+
+func TestReadStepsWithEnv(t *testing.T) {
+	tests := []struct {
+		input  string
+		output int
+	}{
+		{"1", 1},
+		{"2", 2},
+	}
+
+	for _, test := range tests {
+		_ = os.Setenv("STEPS", test.input)
+		result, err := readSteps()
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
 		if result != test.output {
 			t.Errorf("Expected %d, got %d", test.output, result)
 		}
