88b24ba8b4d5526c4b47f02c6b4f1f9727a23796
kamarasd <50073613+kamarasd@users.noreply.github.com>
2025-02-02 18:41:31 +0100

    Solve staircase problem kamarasd

---
+++ kamarasd/staircaseProblem.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"log"
+)
+
+func main() {
+
+	os.Setenv("STEPS", "4")
+	var n int
+
+	n = readChar(n)
+
+	if(n == 0) {
+		fmt.Println("\nUser not defined strings to compare gathering default value from env!!\n")
+		env := os.Getenv("STEPS")		
+		m, err := strconv.Atoi(env)
+		if(err != nil) {
+			log.Fatal("ERROR OCCURED TERMINATE")
+		}
+		n = m
+	}
+	
+	fmt.Println("Steps :", n, " Total number of ways: ", calc(n))
+}
+
+func calc(n int) int {
+
+	 switch {
+		case n < 1:
+			return 1
+		case n <= 2:
+			return 2
+		default: 
+			return ((n-1) + (n-2))
+	 }
+}
+
+func readChar(n int) (int) {
+
+	fmt.Println("enter the number of steps: ")
+	fmt.Scanf("%d", &n)
+
+	return n
+} 
+
+
