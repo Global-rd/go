d6e078aa21b205f3b45a18fbab35f614baefa5e1
kamarasd <50073613+kamarasd@users.noreply.github.com>
2025-02-09 10:06:25 +0100

    Changes

--- kamarasd/staircaseProblem.go
+++ kamarasd/staircaseProblem.go
@@ -2,27 +2,18 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strconv"
-	"log"
 )
 
 func main() {
 
-	os.Setenv("STEPS", "4")
+	var defaultSteps int = 4
 	var n int
 
 	n = readChar(n)
 
-	if(n == 0) {
-		fmt.Println("\nUser not defined strings to compare gathering default value from env!!\n")
-		env := os.Getenv("STEPS")		
-		fmt.Println("\nUser not defined any steps to calculate, gathering default value from env!!\n")
-		m, err := strconv.Atoi(env)
-		if(err != nil) {
-			log.Fatal("ERROR OCCURED TERMINATE")
-		}
-		n = m
+	if n == 0 {
+		fmt.Println("User not defined any steps to calculate, gathering default value from!!")
+		n = defaultSteps
 	}
 
 	fmt.Println("Steps :", n, " Total number of ways: ", calc(n))
@@ -30,19 +21,19 @@ func main() {
 
 func calc(n int) int {
 
-	 switch {
-		case n < 1:
-			return 1
-		case n <= 2:
-			return 2
-		default: 
-			return ((n-1) + (n-2))
-	 }
+	switch {
+	case n < 1:
+		return 1
+	case n <= 2:
+		return 2
+	default:
+		return (calc(n-1) + calc(n-2))
+	}
 }
 
 func readChar(n int) int {
 
-	fmt.Println("enter the number of steps: ")
+	fmt.Println("Enter the number of steps: ")
 	fmt.Scanf("%d", &n)
 
 	return n
