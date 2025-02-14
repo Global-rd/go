0b5de90ec927b37b17fe4b7aba08bbc995461914
geer26 <gergo.kurucz@mobilegap.com>
2025-02-14 07:58:54 +0100

    API dockerized

--- geer26/panic_handling/api/main.go
+++ geer26/panic_handling/api/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+)
+
+const (
+	RANDCHARS = "0123456789A"
+)
+
+func random_selection() string {
+	randomIndex := rand.Intn(len(RANDCHARS))
+	return string(RANDCHARS[randomIndex])
+}
 
 func main() {
-	log.Println("Hello world from api!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, fmt.Sprintf("%s", random_selection()))
+	})
+
+	http.ListenAndServe(":5000", nil)
 }
--- geer26/panic_handling/client/main.go
+++ geer26/panic_handling/client/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
 	log.Println("Hello woorld from client!")
+
+	response, err := http.Get("http://localhost:5000/")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
 }
