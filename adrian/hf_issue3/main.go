4bdae13192cd8c0a91315606ea2a5dd1b82617f0
kollarovics <38493273+kollarovics@users.noreply.github.com>
2025-03-05 18:44:02 +0100

    17 recover from panic during data processing adrian (#21)
    * Solution to issue 3, Recover from panic during data processing
    
    * Solution to issue 3, Recover from panic during data processing
    
    * Changes recommended by @huszkacs implemented
    
    * Panix simulation moved from getQuote() into the main loop
    
    * wrapping error in fetchAndWriteQuotes defer part
    
    * wrapping error in fetchAndWriteQuotes defer part
    
    * Defer removed from main
    
    ---------
    
    Co-authored-by: adrian <adrian.kollarovics@mavenir.com>

---
+++ adrian/hf_issue3/main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const (
+	QuoteUrl       = "http://kollarovics.net:54380/quote"
+	OutputFileName = "quotes_output.txt"
+	QuotesToGet    = 50
+	FlushInterval  = 5
+	RndMin         = 3
+	RndMax         = 5
+)
+
+type Writer interface {
+	Write(string) error
+	Flush() error
+	Close() error
+	GetSuccessCount() uint
+}
+
+type BatchWriter struct {
+	WriteSuccess uint
+	Buffer       []Quote
+	OutputFile   os.File
+}
+
+func (bw *BatchWriter) Write(text string) error {
+	quoteParts := strings.Split(text, "~")
+	if len(quoteParts) != 2 {
+		return errors.New("Invalid quote format")
+	}
+	quote := Quote{
+		Text:   strings.TrimRight(quoteParts[0], " "),
+		Author: quoteParts[1],
+	}
+	bw.Buffer = append(bw.Buffer, quote)
+	return nil
+}
+
+func NewBatchWriter() (*BatchWriter, error) {
+	output, err := os.OpenFile(OutputFileName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return nil, err
+	}
+	batchWriter := BatchWriter{
+		WriteSuccess: 0,
+		OutputFile:   *output,
+	}
+	batchWriter.Buffer = make([]Quote, 0)
+	return &batchWriter, nil
+}
+
+func (bw *BatchWriter) Flush() error {
+	for _, quote := range bw.Buffer {
+		_, err := bw.OutputFile.WriteString(fmt.Sprintf("Author: %s, Quote: \"%s\"\n", quote.Author, quote.Text))
+		if err != nil {
+			return err
+		}
+		bw.WriteSuccess++
+	}
+	bw.Buffer = make([]Quote, 0)
+	return nil
+}
+
+func (bw *BatchWriter) Close() error {
+	return bw.OutputFile.Close()
+}
+
+func (bw *BatchWriter) GetSuccessCount() uint {
+	return bw.WriteSuccess
+}
+
+type Quote struct {
+	Text   string
+	Author string
+}
+
+func getQuote() (quote string, err error) {
+	var resp *http.Response
+	resp, err = http.Get(QuoteUrl)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		return "", errors.New("Quote fetch failed")
+	}
+	defer func() {
+		err = resp.Body.Close()
+		if err != nil {
+			fmt.Printf("Error closing response body: %v", err)
+		}
+	}()
+	var body []byte
+	body, err = io.ReadAll(resp.Body)
+	quote = string(body)
+	return quote, err
+}
+
+func shouldSimulatePanic(idx int) bool {
+	// Checks randomly if the bits 3-5 are set in the loop index
+	// if yes, a panic should be simulated
+	bitIdx := rand.Intn(RndMax-RndMin+1) + RndMin
+	checkBit := 1 << bitIdx
+	return (checkBit & idx) != 0
+}
+
+func fetchAndWriteQuotes(writer Writer) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("Panic occured: %v\n", r)
+			fmt.Printf("So far %d quotes written. Flushing remaining quotes from bufer\n", writer.GetSuccessCount())
+			err2 := writer.Flush()
+			if err2 != nil {
+				err = fmt.Errorf("panic: %v, followed by flush error: %v\n", r, err2)
+			} else {
+				err = fmt.Errorf("panic occured: %v\n", r)
+			}
+		}
+	}()
+	for i := 0; i < QuotesToGet; i++ {
+		var quote string
+		quote, err = getQuote()
+		err = writer.Write(quote)
+		if err != nil {
+			fmt.Printf("Error writing quote: %v\n", err)
+			return err
+		}
+		if shouldSimulatePanic(i) {
+			panic("Simulated panic")
+		}
+		if (i+1)%FlushInterval == 0 {
+			err = writer.Flush()
+			if err != nil {
+				fmt.Printf("Error flushing quotes: %v\n", err)
+				return err
+			}
+			fmt.Printf("So far %d quotes written\n", writer.GetSuccessCount())
+		}
+	}
+	err = writer.Flush()
+	if err != nil {
+		fmt.Printf("Error flushing quotes: %v\n", err)
+		return err
+	}
+	return err
+}
+
+func main() {
+	fmt.Printf("Application starting, trying to get %d quotes\n", QuotesToGet)
+	batchWriter, err := NewBatchWriter()
+	if err != nil {
+		fmt.Printf("Error initializing batch writer: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = fetchAndWriteQuotes(batchWriter)
+	closeErr := batchWriter.Close()
+	if closeErr != nil {
+		fmt.Printf("Error closing output file: %v\n", closeErr)
+	}
+	if err != nil {
+		fmt.Printf("Error occured while fetching quotes: %v\nWritten quotes: %d\n,", err, batchWriter.WriteSuccess)
+		os.Exit(1)
+	}
+	fmt.Printf("Application finished, wrote %d quotes\n", batchWriter.WriteSuccess)
+}
