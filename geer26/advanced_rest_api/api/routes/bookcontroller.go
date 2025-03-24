fa122ab0baa12fdafa74293a6a64f3a6cb7951e2
geer26 <gergo.kurucz@mobilegap.com>
2025-03-24 08:08:12 +0100

    db controller

--- geer26/advanced_rest_api/api/routes/bookcontroller.go
+++ geer26/advanced_rest_api/api/routes/bookcontroller.go
@@ -6,14 +6,8 @@ import (
 	"fmt"
 	"net/http"
 
-	. "github.com/doug-martin/goqu/v9"
 	"github.com/go-chi/chi/v5"
-	"github.com/google/uuid"
 )
-
-type EmptyResponse struct {
-	res []db.Book `json:"books"`
-}
 
 type BaseResponse struct {
 	Status  int    `json:"status"`
@@ -24,24 +18,10 @@ type BookController struct {
 }
 
 func (b BookController) ListBooks(w http.ResponseWriter, r *http.Request) {
-	rows, err := DbConnection.Query(`SELECT * FROM bookshelf;`)
+	result, err := db.GetAllBooks(DbConnection)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
-	}
-
-	defer rows.Close()
-
-	var result []db.Book
-
-	for rows.Next() {
-		var book db.Book
-		err := rows.Scan(&book.Id, &book.Title, &book.Author, &book.Published, &book.Introduction, &book.Price, &book.Stock)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		result = append(result, book)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -54,30 +34,10 @@ func (b BookController) GetBooks(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid ID", http.StatusBadRequest)
 		return
 	}
-	ds := From("bookshelf").Where(C("id").Eq(id))
-	expression, _, _ := ds.ToSQL()
 
-	rows, err := DbConnection.Query(expression)
+	result, err := db.GetBook(DbConnection, id)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var result []db.Book
-
-	for rows.Next() {
-		var book db.Book
-		err := rows.Scan(&book.Id, &book.Title, &book.Author, &book.Published, &book.Introduction, &book.Price, &book.Stock)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		result = append(result, book)
-	}
-
-	if len(result) == 0 {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(EmptyResponse{})
 		return
 	}
 
@@ -93,29 +53,14 @@ func (b BookController) CreateBook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	new_book.Id = string(uuid.New().String())
-
-	ds := Insert("bookshelf").Rows(
-		Record{
-			"id":           new_book.Id,
-			"title":        new_book.Title,
-			"author":       new_book.Author,
-			"published":    new_book.Published,
-			"introduction": new_book.Introduction,
-			"price":        new_book.Price,
-			"stock":        new_book.Stock,
-		},
-	)
-	expression, _, _ := ds.ToSQL()
-
-	_, err = DbConnection.Query(expression)
+	result, err := db.InsertBook(DbConnection, &new_book)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(new_book)
+	json.NewEncoder(w).Encode(result)
 }
 
 func (b BookController) UpdateBook(w http.ResponseWriter, r *http.Request) {
@@ -134,32 +79,40 @@ func (b BookController) UpdateBook(w http.ResponseWriter, r *http.Request) {
 
 	new_book.Id = id
 
-	ds := From("bookshelf").
-		Where(C("id").Eq(id)).
-		Update().
-		Set(
-			Record{
-				"id":           new_book.Id,
-				"title":        new_book.Title,
-				"author":       new_book.Author,
-				"published":    new_book.Published,
-				"introduction": new_book.Introduction,
-				"price":        new_book.Price,
-				"stock":        new_book.Stock,
-			},
-		)
-	expression, _, _ := ds.ToSQL()
-
-	_, err = DbConnection.Query(expression)
+	response, err := db.UpdateBook(DbConnection, &new_book)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	response := BaseResponse{
-		Status:  1,
-		Message: fmt.Sprintf("Book %s updated", id),
-	}
+	/*
+		ds := From("bookshelf").
+			Where(C("id").Eq(id)).
+			Update().
+			Set(
+				Record{
+					"id":           new_book.Id,
+					"title":        new_book.Title,
+					"author":       new_book.Author,
+					"published":    new_book.Published,
+					"introduction": new_book.Introduction,
+					"price":        new_book.Price,
+					"stock":        new_book.Stock,
+				},
+			)
+		expression, _, _ := ds.ToSQL()
+
+		_, err = DbConnection.Query(expression)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		response := BaseResponse{
+			Status:  1,
+			Message: fmt.Sprintf("Book %s updated", id),
+		}
+	*/
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
@@ -171,10 +124,7 @@ func (b BookController) DeleteBook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ds := Delete("bookshelf").Where(C("id").Eq(id))
-	expression, _, _ := ds.ToSQL()
-
-	_, err := DbConnection.Query(expression)
+	err := db.DeleteBook(DbConnection, id)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
