4bff70e2997083454660f15e443d5b41f55951f4
geer26 <gergo.kurucz@mobilegap.com>
2025-04-16 16:10:13 +0200

    siplify

--- geer26/concurrency/routes/main.go
+++ geer26/concurrency/routes/main.go
@@ -1,108 +1,35 @@
 package routes
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"log"
 	"main/utils"
 	"net/http"
 	"time"
 )
 
-type Signal struct{}
-
-type ReturnStruct struct {
-	CatFact        string `json:"cat_fact"`
-	UselessFact    string `json:"useless_fact"`
-	BullshitExcuse string `json:"bullshit_excuse"`
-	Status         int    `json:"status"`
-	Errors         []string
-}
-
-type ChannelStruct struct {
-	Fetcher func() (string, error)
-	Message chan string
-	Done    bool
-	Err     chan error
+type Uselessfact struct {
+	UselessFact string `json:"useless_fact"`
 }
 
 func BaseHandler(w http.ResponseWriter, r *http.Request) {
-	var retval = ReturnStruct{
-		Status: 1,
+	start := time.Now()
+
+	uselessfact := utils.Getuselessfact(context.Background())
+
+	select {
+	case fetched := <-uselessfact:
+		log.Println("Time elapsed: ", time.Since(start))
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(fetched)
+		return
+	case <-time.After(time.Millisecond * 30):
+		log.Println("Time elapsed: ", time.Since(start))
+		w.WriteHeader(http.StatusRequestTimeout)
+		fmt.Fprintf(w, "request timed out")
 	}
 
-	done := make(chan Signal)
-
-	catFact := ChannelStruct{
-		Fetcher: utils.GetCatFact,
-		Message: make(chan string),
-		Done:    false,
-	}
-
-	uselessFact := ChannelStruct{
-		Fetcher: utils.GetUselessFact,
-		Message: make(chan string),
-		Done:    false,
-	}
-
-	bullshitExcuse := ChannelStruct{
-		Fetcher: utils.GetBullshitExcuse,
-		Message: make(chan string),
-		Done:    false,
-	}
-
-	go func() {
-		ApplyFetchers(&catFact, &uselessFact, &bullshitExcuse)
-	}()
-
-	go func() {
-		defer close(done)
-		for {
-			if catFact.Done && uselessFact.Done && bullshitExcuse.Done {
-				return
-			}
-			select {
-			case v := <-catFact.Message:
-				retval.CatFact = v
-			case v := <-catFact.Err:
-				retval.Errors = append(retval.Errors, v.Error())
-				retval.Status = -1
-			case v := <-uselessFact.Message:
-				retval.UselessFact = v
-			case v := <-uselessFact.Err:
-				retval.Errors = append(retval.Errors, v.Error())
-				retval.Status = -1
-			case v := <-bullshitExcuse.Message:
-				retval.BullshitExcuse = v
-			case v := <-bullshitExcuse.Err:
-				retval.Errors = append(retval.Errors, v.Error())
-				retval.Status = -1
-			case <-time.After(time.Millisecond * 2000):
-				retval.Status = -1
-				retval.Errors = append(retval.Errors, "timed out")
-				return
-			}
-		}
-	}()
-
-	<-done
-
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(retval)
-}
-
-func ApplyFetchers(fetchers ...*ChannelStruct) {
-
-	for _, fetcher := range fetchers {
-		go func() {
-			fetched, err := fetcher.Fetcher()
-			if err != nil {
-				fetcher.Err <- err
-				fetcher.Done = true
-				return
-			}
-			fetcher.Message <- fetched
-			fetcher.Done = true
-			return
-		}()
-	}
 }
--- geer26/concurrency/utils/fetchers.go
+++ geer26/concurrency/utils/fetchers.go
@@ -1,18 +1,10 @@
 package utils
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
-	"math/rand"
 	"net/http"
-	"strconv"
 )
-
-type BullshitExcuse struct {
-	Bs    string `json:"bs"`
-	Param string `json:"param"`
-	Size  string `json:"size"`
-}
 
 type UselessFact struct {
 	Id        string `json:"id"`
@@ -23,52 +15,21 @@ type UselessFact struct {
 	Text      string `json:"text"`
 }
 
-type CatFact struct {
-	Data []string `json:"data"`
-}
-
-func GetCatFact() (string, error) {
-	catfactUrl := "https://meowfacts.herokuapp.com/"
-	resp, err := http.Get(catfactUrl)
-	if err != nil {
-		return "", err
-	}
-	var catfact CatFact
-	err = json.NewDecoder(resp.Body).Decode(&catfact)
-	if err != nil {
-		return "", err
-	}
-	return catfact.Data[0], nil
-}
-
-func GetUselessFact() (string, error) {
-	uselessfactUrl := "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
-	resp, err := http.Get(uselessfactUrl)
-	if err != nil {
-		return "", err
-	}
-	var uselessfact UselessFact
-	err = json.NewDecoder(resp.Body).Decode(&uselessfact)
-	if err != nil {
-		return "", err
-	}
-	return uselessfact.Text, nil
-}
-
-func GetBullshitExcuse() (string, error) {
-	var randexcuse string
-	for _ = range 4 {
-		randexcuse += strconv.Itoa(rand.Intn(10000))
-	}
-	bullshitExcuseUrl := fmt.Sprintf("https://bullshit.sandros.hu/excuse/getbs.php?randy=0.%s", randexcuse)
-	resp, err := http.Get(bullshitExcuseUrl)
-	if err != nil {
-		return "", err
-	}
-	var bullshitexcuse BullshitExcuse
-	err = json.NewDecoder(resp.Body).Decode(&bullshitexcuse)
-	if err != nil {
-		return "", err
-	}
-	return bullshitexcuse.Bs, nil
+func Getuselessfact(ctx context.Context) chan string {
+	ch := make(chan string)
+	uselessfact := UselessFact{}
+	go func() {
+		defer close(ch)
+		uselessfactUrl := "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
+		resp, err := http.Get(uselessfactUrl)
+		if err != nil {
+			close(ch)
+		}
+		err = json.NewDecoder(resp.Body).Decode(&uselessfact)
+		if err != nil {
+			close(ch)
+		}
+		ch <- uselessfact.Text
+	}()
+	return ch
 }
