a151ad0ad0841fbaf54a65fb15f4bcd1088d8556
geer26 <gergo.kurucz@mobilegap.com>
2025-04-21 16:00:15 +0200

    logger service

---
+++ geer26/containerizing/log_service/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+type LogMessage struct {
+	Log       string    `json:"log"`
+	Type      string    `json:"type"`
+	Timestamp time.Time `json:"timestamp"`
+	ID        string    `json:"id"`
+}
+
+const (
+	KAFKAURL = "localhost:9092"
+	TOPIC    = "log_service"
+)
+
+func getKafkaReader(kafkaURL, topic, groupID string) *kafka.Reader {
+	brokers := strings.Split(kafkaURL, ",")
+
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:   brokers,
+		Topic:     topic,
+		Partition: 0,
+		MaxBytes:  10e6, // 10MB
+		GroupID:   groupID,
+	})
+
+	return reader
+
+}
+
+func main() {
+	done := make(chan struct{})
+
+	go func() {
+		reader := getKafkaReader(KAFKAURL, TOPIC, "logger_service")
+		defer reader.Close()
+
+		for {
+			m, err := reader.ReadMessage(context.Background())
+			if err != nil {
+				log.Fatalf("error at reading queue: %s", err.Error())
+			}
+			fmt.Printf("message: %s\n", string(m.Value))
+		}
+
+	}()
+
+	<-done
+}
